67f6dc091fba50f3e7bc979f335507b7b47fba58
gui <guisimoes2010@gmail.com>
2026-05-27 15:30:43 -0300

    Set default baseURL for pokeApi

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const pokeApi = axios.create ({
-    baseURL: process.env.POKE_API_URL,
+    baseURL: process.env.POKE_API_URL || "https://pokeapi.co/api/v2/",
 });
-
